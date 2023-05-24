ba65208f77b5ae2a2ab1a2e8faa5b24b27d6fd73
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-24 11:25:03 +0530

    [Madhur]-Fibonnaci Series Added

---
+++ FibonnaciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_LogicalProgramning
+{
+    internal class FibonnaciSeries
+    {
+        public void CheckFibonnaciSeries()
+        {
+            int p1 = 0, p2 = 1, p3;
+            Console.Write("Enter number = ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write(p1 + " " + p2);
+            for (int i = 2; i < n; i++)
+            {
+                p3 = p1 + p2;
+                Console.Write(" " + p3);
+                p1 = p2;
+                p2 = p3;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day6_LogicalProgramning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter option \n1) Fibonnaci Series");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonnaciSeries fibonnaci = new FibonnaciSeries();
+                    fibonnaci.CheckFibonnaciSeries();
+                    break;
+            }
+        }
+    }
+}
